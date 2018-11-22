c25e13831acf8934746a554101e4b6cb2165a122
Michael Prather <mprather1@gmail.com>
2018-11-22 02:58:17 +0000

    removed standard from git command

--- pages/about.js
+++ pages/about.js
@@ -5,53 +5,39 @@ import Title from 'components/Title'
 import Content from 'components/Content'
 import api from 'api/about'
 import actions from 'state/actions/about'
-import mainState from 'state/actions/main'
 
 class About extends React.Component {
-  componentDidMount () {
-    this.props.toggleInProp(true)
+  static async getInitialProps ({ store }) {
+    try {
+      let json = await api.fetch()
+
+      store.dispatch(actions.fetch(json))
+    } catch (err) {
+      console.error(err.message)
+    }
+
+    return {
+      custom: 'About!!'
+    }
+  }
+
+  static propTypes = {
+    about: PropTypes.object.isRequired
   }
 
   render () {
-    const { about, main } = this.props
+    const { about } = this.props
 
     return (
       <Main title='about' host='shintech.ninja' favicon='/static/images/react.svg' >
-        {(!main.loaded) ? <span />
-          : <div className='container'>
-            <Title title={about.title} fontSize='24ch' colors={['gold', 'green']} />
-            <Content content={about.data} />
-          </div>
-        }
+        <Title title={about.title} fontSize='24ch' colors={['gold', 'green']} />
+        <Content content={about.data} />
       </Main>
     )
   }
 }
 
-About.getInitialProps = async ({ store }) => {
-  store.dispatch(mainState.toggleInProp(false))
-
-  try {
-    let json = await api.fetch()
-
-    store.dispatch(actions.fetch(json))
-  } catch (err) {
-    console.error(err.message)
-  }
-
-  return { custom: 'About!!' }
-}
-
-About.propTypes = {
-  about: PropTypes.object.isRequired
-}
-
-const mapStateToProps = (state) => state
-
-const mapDispatchToProps = dispatch => ({
-  toggleInProp: bool => {
-    dispatch(mainState.toggleInProp(bool))
-  }
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(About)
+export default connect(
+  state => state,
+  dispatch => ({ })
+)(About)
